c2b54b7e9a1d411ebf9acfbfd49902dbaae07777
khoaiscity <khoa@iscity.com.my>
2021-04-18 16:32:12 +0800

    member live chat

---
+++ member/environment.prod.ts
@@ -0,0 +1,28 @@
+export const environment = {
+    SERVER_URL: `./`,
+    RESOURCE_SERVER_URL: `./resources-url`,
+    production: false,
+    useHash: true,
+    hmr: false,
+    RUN_MODE: 'TEST',
+    colors: {
+        // color palettes
+        blue: '#4B4BF9',
+        purple: '#7F33AD',
+        cyan: '#00CBD0',
+        green: '#2ECC71',
+        magenta: '#FF3366',
+        // alias of magenta
+        pink: '#FF3366',
+        red: '#E74C3C',
+        orange: '#fa8c16',
+        yellow: '#FDDD0E',
+        volcano: '#fa541c',
+        geekblue: '#26B1FD',
+        lime: '#a0d911',
+        gold: '#faad14',
+        grey: '#888576',
+        brown: '#A46333',
+        textColor: '#333'
+    }
+};
