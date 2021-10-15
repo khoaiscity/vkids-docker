13a33df45ca17d8f26eb02ae765317f0db8f2a6d
datiscity <dat@iscity.com.my>
2021-10-15 15:05:12 +0800

    admin evironment

--- admin/build.Dockerfile
+++ admin/build.Dockerfile
@@ -8,9 +8,11 @@ RUN mkdir $APP_HOME
 WORKDIR $APP_HOME
 
 COPY ./config.ts $APP_HOME/
+COPY ./environment.prod.ts $APP_HOME/
 
 CMD echo BUILDING... \
   && cp config.ts mlm-ng-admin/client/src/app/ \
+  && cp environment.prod.ts mlm-ng-admin/client/src/environments/ \
   && npm install --no-color --silent --prefix mlm-ng-admin/client \
   && npm run build --progress=false --no-color --prefix mlm-ng-admin/client \
   && cd mlm-ng-admin \
--- admin/environment.prod.ts
+++ admin/environment.prod.ts
@@ -1,8 +1,11 @@
 export const environment = {
-    SERVER_URL: `./`,
-    RESOURCE_SERVER_URL: `./resources-url`,
-    production: true,
-    useHash: true,
-    hmr: false,
-    RUN_MODE: 'TEST',
+  SERVER_URL: `./`,
+  RESOURCE_SERVER_URL: `./resources-url`,
+  production: true,
+  useHash: true,
+  hmr: false,
+  RUN_MODE: 'Production',
+  liveChatPrivateKey: 'rSSqvZ5AMLDwM3Cg8uAKUDqCnPMf5BjdvNshbkka',
+  liveChatPortMember: '2028',
+  liveChatPortAdmin: '2228',
 };
